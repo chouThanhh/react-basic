c991b5ed94eb50bbaf442884f3db491cbab41962
tuhoc.cc <ksgoku2004@gmail.com>
2025-02-23 23:04:32 +0700

    10 quan he cha con react - nestin components

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ChildComponent from "./ChildComponent";
 
 class MyComponent extends React.Component {
   //key:value
@@ -51,6 +52,10 @@ class MyComponent extends React.Component {
             onClick={(event) => this.handleSubmit(event)}
           />
         </form> 
+
+        <ChildComponent name={'child one'}/>
+        <ChildComponent name={'child two'}/>
+        <ChildComponent name={'child three'}/>
       </>
     );
   }
