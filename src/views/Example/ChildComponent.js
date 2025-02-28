787d5a4a486ac48073d2453c6f058e7b982aeafc
tuhoc.cc <ksgoku2004@gmail.com>
2025-02-28 11:49:10 +0700

    14 Conditional output - Sử dụng điều kiện với Reactjs

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,57 +1,76 @@
 import React from "react";
 
-// class ChildComponent extends React.Component {
+class ChildComponent extends React.Component {
+  //re-render
+  state={
+    showJobs: false
+  }
+  handleShowHide = () => {
+    this.setState({
+      showJobs: !this.state.showJobs
+    })
+  }
+  render() {
+    let {arrJobs} = this.props;
+    let {showJobs} = this.state;
+    let check = showJobs === true ? 'showJobs = true' : 'showJobs = false'
+    console.log('>>> check conditional: ', check)
+    return (
+      <>
+        {showJobs === false ?
+          <div>
+            <button onClick={() => this.handleShowHide()}>Show</button>
+          </div>
+        :
+          <>
+          <div className="job-lists">
+            {
+              arrJobs.map((item, index) => {
+                return (
+                    <div key={item.id}>
+                        {item.title} - {item.salary}
+                    </div>
+                )
+              })
 
-//   render() {
-//     console.log(">>> check props: ", this.props);
-//     // let name = this.props.name;
-//     // let age = this.props.age;
-//     // key:value
+            }
 
-//     let {name, age, address, arrJobs} = this.props;
+          </div>
+          <div>
+            <button onClick={() => this.handleShowHide()}>Hide</button>
+          </div>
+          </> 
+        }
+      </>
+    );
+
+  }
+}
+
+// const ChildComponent = (props) => {
+  
+//     let { arrJobs } = props;
 
 //     return (
 //       <>
 //         <div className="job-lists">
 //           {
 //             arrJobs.map((item, index) => {
-//               return (
-//                   <div key={item.id}>
-//                       {item.title} - {item.salary}
-//                   </div>
-//               )
+//               if(+item.salary >= 500){
+
+//                 return (
+//                     <div key={item.id}>
+//                         {item.title} - {item.salary} $
+//                     </div>
+//                 )
+//               }
 //             })
 
 //           }
 
 //         </div>
 //       </>
-//     );
-
-//   }
+//     )
 // }
-
-const ChildComponent = (props) => {
-  
-    let { arrJobs } = props;
-
-    return (
-      <>
-        <div className="job-lists">
-          {
-            arrJobs.map((item, index) => {
-              return (
-                  <div key={item.id}>
-                      {item.title} - {item.salary}
-                  </div>
-              )
-            })
-
-          }
-
-        </div>
-      </>
-    )
-}
 
 export default ChildComponent; 
