0b128b635f77519023185c0bb1efb198ceee0019
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-02 16:43:05 +0700

    21.1 Xây dựng Todo APP PRO với Reactjs - CREATE/READ

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 /*
 * 2 components: class component/ funciotn component(function, arrow)
@@ -14,12 +17,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world with React.js PDK
+          Simple TODO Apps with with React.js
         </p>
-        
-        <MyComponent />
 
+        {/* <MyComponent /> */}
+        <ListTodo />
       </header>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      // transition={Bounce}
+      />
     </div>
   );
 }
