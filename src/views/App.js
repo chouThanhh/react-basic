bb58f154d71849603c1c36b4394c1a459ad152ee
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-03 20:46:34 +0700

    22 React Routers - Điều hướng trang với React

--- src/views/App.js
+++ src/views/App.js
@@ -5,6 +5,15 @@ import ListTodo from './Todos/ListTodo';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Nav from './Nav/nav';
+import Home from './Example/Home';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 /*
 * 2 components: class component/ funciotn component(function, arrow)
 * JSX
@@ -13,31 +22,44 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   //const App = () => {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Simple TODO Apps with with React.js
-        </p>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
 
-        {/* <MyComponent /> */}
-        <ListTodo />
-      </header>
+          {/* <MyComponent /> */}
+          {/* <ListTodo /> */}
 
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      // transition={Bounce}
-      />
-    </div>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/todo">
+              <ListTodo />
+            </Route>
+            <Route path="/about">
+              <MyComponent />
+            </Route>
+          </Switch>
+
+        </header>
+
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        // transition={Bounce}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
