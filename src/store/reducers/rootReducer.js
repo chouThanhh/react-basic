eb691454ef63117510de081522204d255ad6ea9d
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-07 11:39:23 +0700

    32 Setup Redux vào dự án React

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    user: [
+        { id: 1, name: 'Eric' },
+        { id: 2, name: 'Hoi Dan IT' }
+    ]
+}
+const rootReducer = (state = initState, action) => {
+
+    return state;
+}
+
+export default rootReducer;
